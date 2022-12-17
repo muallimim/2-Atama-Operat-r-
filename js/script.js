365dd1f7d470af1357cd8b4e5b7e6dcd3d3957bf
muallimim <muallimim@yahoo.com>
2022-12-17 21:51:12 +0300

    Ekrana merhaba yazdırıldı

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+var text;
+var text2;
+var sonuc;
+
+sonuc = "M";
+sonuc += "e";
+//sonuc = sonuc + "e";
+sonuc += "r";
+sonuc += "h";
+sonuc += "a";
+sonuc += "b";
+sonuc += "a";
+
+
+document.write(sonuc);
+//document.getElementById('demo').innerHTML = sonuc;
+
